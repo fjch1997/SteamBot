1cc6ba3e37873f6c91bae77c5b668ad9fb5f31b1
Lloyd Feng <fjch1997@outlook.com>
2018-10-30 12:51:43 -0400

    SubmitCookies

--- SteamTrade/ISteamWeb.cs
+++ SteamTrade/ISteamWeb.cs
@@ -26,6 +26,7 @@ namespace SteamTrade
         /// Session id of Steam after Login.
         /// </summary>  
         [Obsolete("Session ID can be different between store.steampowered.com and steamcommunity.com. Use Cookies.GetCookies(new Uri(\"https://steamcommunity.com/\")).Cast<Cookie>().FirstOrDefault(c => c.Name == \"sessionid\")?.Value to get Session ID for the domain you are working with.")]
+        string SessionId { get; }
         /// <summary>
         /// Token secure as string. It is generated after the Login.
         /// </summary>
